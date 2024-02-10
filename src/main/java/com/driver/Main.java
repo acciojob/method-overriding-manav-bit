60165df2bb6a826f46827b0ffb5c02d9479594e4
MANAV AHUJA <ahujamanav43@gmail.com>
2024-02-10 23:39:47 +0530

    ans done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,21 @@
 package com.driver;
 
 public class Main {
-  
+
+    public static void main(String args[]){
+        B obj=new B();
+        obj.meth();
+    }
+    public static class A{
+        public String meth(){
+            String ans="Invoking method from class A";
+          return  ans;
+        }
+    }
+    public static class B extends A{
+        @Override
+        public String meth(){
+            return "class B function";
+        }
+    }
 }
